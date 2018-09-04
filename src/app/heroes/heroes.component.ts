f79c12c8978f4cccd3de876df46042e16911e830
RossPin <dorpetz@hotmail.com>
2018-09-04 16:30:17 +1200

    add hero service to provide hero data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero'
-import {HEROES} from '../mock-heroes'
+import {HeroService} from '../hero.service'
 
 
 @Component({
@@ -10,14 +10,19 @@ import {HEROES} from '../mock-heroes'
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes()
   }
 
-  heroes = HEROES
-
+  heroes: Hero[]
   selectedHero: Hero
+
+  getHeroes(): void{
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero
